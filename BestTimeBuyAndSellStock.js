7cc7065f8d1bccbe422d51f75a39242ceccf7af3
rizalbazim3-ctrl <rizalbazim3@gmail.com>
2026-07-17 12:08:47 +0530

    BestTimeBuyAndSellStock

---
+++ BestTimeBuyAndSellStock.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    
+   let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+
+        let profit = prices[i] - minPrice;
+
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+    }
+
+    return maxProfit;
+      
+};
