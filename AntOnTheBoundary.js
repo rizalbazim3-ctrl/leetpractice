867d63c988e1a8a9dd6061e44d712f3293039e76
rizalbazim3-ctrl <rizalbazim3@gmail.com>
2026-07-25 11:53:48 +0530

    ant on the boundary

---
+++ AntOnTheBoundary.js
@@ -0,0 +1,25 @@
+// An ant is on a boundary. It sometimes goes left and sometimes right.
+
+// You are given an array of non-zero integers nums.
+//  The ant starts reading nums from the first element of it to its end. At each step, 
+//  it moves according to the value of the current element:
+
+// If nums[i] < 0, it moves left by -nums[i] units.
+// If nums[i] > 0, it moves right by nums[i] units.
+// Return the number of times the ant returns to the boundary.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var returnToBoundaryCount = function(nums) {
+    let a = 0
+    let count = 0
+    for(let int of nums){
+        a+=int
+        if(a === 0){
+             count++
+        }
+    }
+    return  count 
+};
