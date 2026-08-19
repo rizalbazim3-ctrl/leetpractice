6e790dafa9e62fa81244d8e46de3809dda437cf7
rizalbazim3-ctrl <rizalbazim3@gmail.com>
2026-08-19 15:30:17 +0530

    Majority Element

--- MajorityElement.js
+++ MajorityElement.js
@@ -1,3 +1,15 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋
+//  times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+
 /**
  * @param {number[]} nums
  * @return {number}
@@ -6,6 +18,8 @@ var majorityElement = function(nums) {
     let a = [...new Set(nums.filter((value)=> nums.indexOf(value) !== nums.lastIndexOf(value)))]
     if(a.length === 1){
         return a[0]
+    }else if(nums.length === 1){
+        return nums[0]
     }
     let b = []
     for(let i=0; i<a.length; i++){
@@ -18,7 +32,5 @@ var majorityElement = function(nums) {
         b.push(count)
     }
     let c = b.indexOf(Math.max(...b))
-    console.log(a)
     return a[c]
 };
-console.log(majorityElement([2]))
