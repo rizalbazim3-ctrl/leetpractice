a86922b4dd1fca09e912563249e5ce3e361d31ad
rizalbazim3-ctrl <rizalbazim3@gmail.com>
2026-09-03 09:30:17 +0530

    distribute candies

---
+++ DistributeCandies.js
@@ -0,0 +1,28 @@
+// Alice has n candies, where the ith candy is of type candyType[i]. 
+// Alice noticed that she started to gain weight, so she visited a doctor.
+
+// The doctor advised Alice to only eat n / 2 of the candies she 
+// has (n is always even). Alice likes her candies very much, and she wants to
+//  eat the maximum number of different types of candies while still following the doctor's 
+//  advice.
+
+// Given the integer array candyType of length n, return the maximum 
+// number of different types of candies she can eat if she only eats n / 2 of them.
+
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+    let count = 0
+
+    let u = [...new Set(candyType)]
+    let a = candyType.length/2
+    
+    if(u.length >= a){
+        return a
+    }else {
+        return u.length
+    }
+    
+};
