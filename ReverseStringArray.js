47450471d248feb45d5fcd533446891bffe65e37
rizalbazim3-ctrl <rizalbazim3@gmail.com>
2026-09-13 12:35:59 +0530

    reverse string

---
+++ ReverseStringArray.js
@@ -0,0 +1,18 @@
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    return s.reverse()
+};
