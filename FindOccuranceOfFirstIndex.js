961a2d6f03e9552639885149d914448b89ea1e18
rizalbazim3-ctrl <rizalbazim3@gmail.com>
2026-09-18 10:30:39 +0530

    find the first occurance of a string

---
+++ FindOccuranceOfFirstIndex.js
@@ -0,0 +1,21 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let a = haystack.indexOf(needle)
+   
+    return a
+};
