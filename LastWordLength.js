6e4b6ecdc3bb0dece6a206b889129d4c4aa0a020
rizalbazim3-ctrl <rizalbazim3@gmail.com>
2026-09-18 10:14:30 +0530

    last word length in a string

---
+++ LastWordLength.js
@@ -0,0 +1,21 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let lasts = s.trim().split(" ").at(-1)
+    let lengthoflasts = lasts.trim().length
+    return lengthoflasts
+};
